0c27555045095915c53005d57ca968bb214ea688
EmilySakata <milliemillie@gmail.com>
2017-06-28 22:26:16 -0700

    Add files via upload

--- rps.py
+++ rps.py
@@ -1,44 +1,51 @@
 #homework 1, rps.py
-
 #initial veriable definition
 balance = 50
 wins = 0
 losses = 0
 ties= 0
 
-computerHand = ["r"]
-computerWin = ["p"]
-computerLose =["s"]
+computerHand = "r"
+computerWin = "p"
+computerLose ="s"
 
-#print the initial balance
+# print current balance
+def currentBalance():
+	print ("you have $" +str(balance))
+	print("wins :" + str(wins))
+	print("losses :" + str(losses))
+	print("ties :" + str(ties))
+
+# define funciton
+
+
+# Run the program
+#print the initial balance 
+
 print("you have $"+ str(balance))
-
 # get the input from player
 
 player = input ("Chose r, p or s")
 
-# print result of the game
-if (player in computerHand):
+
+if (player == computerHand):
 	print("you tied with the computer!")
 	ties = ties +1
 
-if(player in computerLose):
+if(player == computerLose):
 	print ("Computer beat you")
 	losses = losses +1
 	balance = balance -10
 
-if (player in computerWin):
+if (player == computerWin):
 	wins= wins+1
 	balance = balance +10
 
-# print current balance
-print ("you have $" +str(balance))
-print("wins :" + str(wins))
-print("losses :" + str(losses))
-print("ties :" + str(ties))
+currentBalance()
+
 
 #check if you have enough balance
-while(balance >0):
+while(balance >0) and (balance != 0):
 
 # play again or not
 	PlayAgain = input("play again ? y or n")
@@ -52,28 +59,27 @@ while(balance >0):
 		player = input ("Chose r, p or s")
 
 # print result of the game
-		if (player in computerHand):
+		
+		if (player == computerHand):
 			print("you tied with the computer!")
 			ties = ties +1
 
-		if(player in computerLose):
+		if(player == computerLose):
 			print ("Computer beat you")
 			losses = losses +1
 			balance = balance -10
 
-		if (player in computerWin):
+		if (player == computerWin):
 			wins= wins+1
 			balance = balance +10
+		currentBalance()
 
 # print current balance
-		print ("you have $" +str(balance))
-		print("wins :" + str(wins))
-		print("losses :" + str(losses))
-		print("ties :" + str(ties))
+		
 	else:
 		print("thanks for playing the game.")
 		exit()
 
-	if (balance ==0):
-		print ("You do not have enough money! You are done.")
+
+print ("You do not have enough money! You are done.")
 # play until your balance is 0 or you end the game
